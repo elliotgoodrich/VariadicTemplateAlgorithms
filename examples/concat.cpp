41e8940f0667b460f4632f88fd7d74969f190bc2
Elliot Goodrich <elliotgoodrich@gmail.com>
2015-01-15 00:27:21 +0000

    Tidy up concat example

--- examples/concat.cpp
+++ examples/concat.cpp
@@ -9,11 +9,12 @@ std::size_t string_size(std::string const& str) noexcept { return str.size(); }
 std::size_t string_size(char const* str) noexcept { return std::strlen(str); }
 std::size_t string_size(char) noexcept { return 1; }
 
+auto const sum = [](auto lhs, auto rhs){ return lhs + rhs; };
+
 template <typename... Args>
 std::string concat(Args&&... args) {
-	auto const total_size = vta::foldl([](auto lhs, auto rhs){ return lhs + rhs; })(0ul, string_size(args)...);
 	std::string str;
-	str.reserve(total_size);
+	str.reserve(vta::foldl(sum)(0ul, string_size(args)...));
 	vta::map([&str](auto const& append){ str += append; })(args...);
 	return str;
 }
